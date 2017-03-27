3d851476d921f1253086318b669521678b35c797
Steve <stevenjchang@gmail.com>
2017-03-27 14:28:35 -0700

    started Grub, Bee, HoneyMakerBee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,26 @@
 var Bee = function() {
+  // Grub.call(this)
+
+  // this.age = 5;
+  this.color = 'yellow';
+  // this.food = inherited;
+  this.eat; //= function inherited;
+  this.job; // = set to keep growing
+
+  return this;
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+Bee.prototype.age = 5;
+
+// Bee.prototype.
+// Create a Bee class, in pseudoclassical style, with:
+// call the Grub superclass
+// set the prototype
+// set the constructor
+// an age property that is set to 5
+// a color property that is set to yellow
+// a food property that is inherited from grub
+// an eat method that is inherited from grub
+// a job property that is set to keep on growing
--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,17 @@
 var Grub = function() {
+  this = Object.create(Grub.prototype);
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
+  this.eat = function(){};
+  return this;
 };
 
+// Grub.prototype = Object.create();
+// Grub.
+
+// Create a Grub class, in pseudoclassical style, with:
+// an age property that is set to 0
+// a color property that is set to pink
+// a food property that is set to jelly
+// an eat method
