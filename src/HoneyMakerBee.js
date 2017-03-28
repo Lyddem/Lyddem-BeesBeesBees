8b2884fad433730897591b590a6a870f245e2ab7
Lyddem <lydiademi@gmail.com>
2017-03-28 16:21:56 -0700

    All tests pass

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -2,14 +2,7 @@ var HoneyMakerBee = function() {
   Bee.call(this);
   this.age = 10;
   this.job = 'make honey';
-
   this.honeyPot = 0;
-  this.makeHoney = function(){
-    this.honeyPot ++;
-  };
-  this.giveHoney = function(){
-    this.honeyPot--;
-  }
 
 };
 
@@ -19,6 +12,14 @@ var HoneyMakerBee = function() {
 
 HoneyMakerBee.prototype = Object.create(Bee.prototype);
 HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function(){
+    this.honeyPot++;
+  };
+HoneyMakerBee.prototype.giveHoney = function(){
+    this.honeyPot--;
+  }
+
 
 // Create a HoneyMakerBee class, in pseudoclassical style, with:
 // call the Bee superclass
